c07a8edf507ee7ba36ee195486f8288e200979d7
Nolan Marketti <nolan@futuremandigital.com>
2017-04-27 14:28:10 -0500

    Updated Config
    Updated Middleman Config

--- config.rb
+++ config.rb
@@ -32,20 +32,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
 # Helpers
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
@@ -55,6 +41,3 @@ page '/*.txt', layout: false
 #     'Helping'
 #   end
 # end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
