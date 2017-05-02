26afdb1b9948889612eeb70a6cf81332913e9406
Nolan Marketti <nolan@futuremandigital.com>
2017-05-02 18:49:05 -0500

    Center Animation
    Finalized center text fade in animation on click.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -6,7 +6,7 @@ var headers = new TimelineMax();
 headers.from(".slide-in", 1.5, {css:{top:"-20px"}, ease:Power2.easeOut}, .75);
 headers.from(".site-name-bottom", 2, {css:{bottom:"-15px"}, ease:Power2.easeOut}, .75);
 headers.from(".menu-button", .8, {css: {opacity:"0"}, ease:Power2.easeOut}, .50);
-headers.staggerFrom(".main-intro-text, .currently-working", 2.25, {css:{opacity:0, top:"15px"}, ease:Power2.easeOut});
+// headers.to(".content01", 2.25, {css:{opacity:0, top:"15px"}, ease:Power2.easeOut});
 
 
 // White Space
@@ -34,14 +34,19 @@ menu.staggerTo(".nav-item", .5, {css:{opacity:"1", top:"0"}, ease:Power2.easeOut
 });
 
 
-// TweenLite.set(".left", {rotation:-90});
+// Entire Content Animation
+
+TweenLite.set("#content01", {autoAlpha:1}); // set to the first content if wannted
 
 $(".nav-item").each(function(index, element){
     $(this).click(function(){
       TweenMax.to(".top .inner", 1.5, {y:"-16" * (index) + "px", ease:Power2.easeOut1});
       TweenMax.to(".left .inner", 1.5, {y:"-16" * (index) + "px", ease:Power2.easeOut1});
-       TweenMax.to(".right .inner", 1.5, {y:"-16" * (index) + "px", ease:Power2.easeOut1});
-      TweenMax.to(".bottom .inner", 1.5, {y:"-16" * (index) + "px", ease:Power2.easeOut1});
+      TweenMax.to(".right .inner", 1.5, {y:"-16" * (index) + "px", ease:Power2.easeOut1});
+      TweenMax.to(".bottom .inner", 1.5, {y:"-16" * (index) + "px", ease:Power2.easeOut1});      
+      TweenLite.to(".content", .5, {autoAlpha:0});  
+      TweenLite.from("#content0" + (index+1), 1, { css:{marginTop:"5px"} });
+      TweenLite.to("#content0" + (index+1), 1, { autoAlpha:1 }); 
     })
 }) 
 		
